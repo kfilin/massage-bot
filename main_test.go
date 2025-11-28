fbdae279151d9b5edc27e05fb9c725b89d5aa57a
Kirill Filin <kfilin@gmail.com>
2025-11-29 02:52:04 +0300

    fix: complete test cleanup - remove all undefined references

--- main_test.go
+++ main_test.go
@@ -2,9 +2,9 @@ package main
 
 import "testing"
 
-func TestBasic(t *testing.T) {
+func TestAlwaysPasses(t *testing.T) {
 	// This test always passes - just verifies the package compiles
-	if 1 != 1 {
-		t.Error("Math is broken")
+	if true != true {
+		t.Error("Logic is broken")
 	}
 }
