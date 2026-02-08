a805570acf2a685bbbff84fd2482dbd1d205d299
Kirill Filin <kfilin@gmail.com>
2026-02-08 03:31:28 +0300

    fix: correct js variable concatenation in twa button onclick

--- internal/storage/record_template.go
+++ internal/storage/record_template.go
@@ -519,7 +519,7 @@ const adminSearchTemplate = `
                     el.innerHTML = '<div class="patient-name">' + p.name + '</div>' +
                         '<div class="patient-info">ID: ' + p.telegram_id + ' • Визитов: ' + p.total_visits + '</div>' +
                         '<div class="btn-row">' +
-                            '<button onclick="event.stopPropagation(); try { alert(\'Debug: Username={{.BotUsername}}\'); window.Telegram.WebApp.openTelegramLink(\'https://t.me/{{.BotUsername}}?start=manual_\' + p.telegram_id); } catch(e) { alert(\'Error: \' + e); window.open(\'https://t.me/{{.BotUsername}}?start=manual_\' + p.telegram_id, \'_blank\'); }" class="btn-action btn-create">➕ Новая запись</button>' +
+                            '<button onclick="event.stopPropagation(); try { alert(\'Debug: Username={{.BotUsername}}\'); window.Telegram.WebApp.openTelegramLink(\'https://t.me/{{.BotUsername}}?start=manual_\' + \'' + p.telegram_id + '\'); } catch(e) { alert(\'Error: \' + e); window.open(\'https://t.me/{{.BotUsername}}?start=manual_\' + \'' + p.telegram_id + '\', \'_blank\'); }" class="btn-action btn-create">➕ Новая запись</button>' +
                             '<button onclick="event.stopPropagation(); viewPatient(\'' + p.telegram_id + '\')" class="btn-action btn-view">📄 Карта</button>' +
                         '</div>';
                     container.appendChild(el);
